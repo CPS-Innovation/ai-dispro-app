cd121a31eac69e3b1ed175bd6a717442121c5585
Peter Bodnar <peter.bodnar@cps.gov.uk>
2026-03-04 13:22:49 +0000

    fix: import

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -2,12 +2,10 @@ from .health import health
 from .workflow import workflow
 from .ingestion import ingestion
 from .analysis import analysis
-from .setup import setup
 
 __all__ = [
     "health",
     "workflow",
     "ingestion",
     "analysis",
-    "setup",
 ]
